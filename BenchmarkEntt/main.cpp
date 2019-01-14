40d252570f19266dd7216b290cb44aa2d95bb1a3
Alan <alanqdev@gmail.com>
2019-01-14 18:45:50 -0200

    new EnTT benchmark WIP

--- BenchmarkEntt/main.cpp
+++ BenchmarkEntt/main.cpp
@@ -1,66 +1,201 @@
 #include <cstdio>
 #include <chrono>
+#include <climits>
 #include "entt.hpp";
 
+using namespace std; // :trollface:
+
 #define TIME_HERE std::chrono::high_resolution_clock::now();
-#define ELAPSEDuS(time_point) (std::uint32_t)(std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::high_resolution_clock::now()-time_point).count());
+#define ELAPSEDMS(time_point) (float)(std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now()-time_point).count());
 
-struct Position {
-    std::uint64_t x;
-    std::uint64_t y;
+struct Int1
+{
+    int x;
 };
 
-struct Velocity {
-    std::uint64_t x;
-    std::uint64_t y;
+struct Int2
+{
+    int x, y;
 };
+
+struct Int3
+{
+    int x, y, z;
+};
+
+struct Int4
+{
+    int x, y, z, w;
+};
+
+struct Int5
+{
+    int x, y, z, w, v;
+};
+
+struct Int6
+{
+    int x, y, z, w, v, u;
+};
+
+int state = 42;
+
+int xorshift(int maxExclusive = INT_MAX)
+{
+    int x = state;
+    x ^= x << 13;
+    x ^= x >> 17;
+    x ^= x << 5;
+    state = x;
+    return abs(x%maxExclusive);
+}
+
+void xorshuffle(std::vector<int> values)
+{
+    for (int i = 0; i < values.size(); i++)
+    {
+        auto swapFor = xorshift(values.size());
+        auto temp = values[i];
+        values[i] = values[swapFor];
+        values[swapFor] = temp;
+    }
+}
+
+
+auto lastTimePoint = TIME_HERE;
+
+static void Measure(char* previousMeasurement = nullptr)
+{
+    if (previousMeasurement != nullptr) printf("%5.2f ms %s", ELAPSEDMS(lastTimePoint), previousMeasurement);
+    lastTimePoint = TIME_HERE;
+}
 
 using namespace entt;
 
+void Benchmark(int entityCount, bool randomComponents);
+
 int main()
 {
-    printf("starting tests...\n");
+    Benchmark(100000, true);
+    Benchmark(100000, true);
 
-    Registry<std::uint32_t> registry;
-    registry.prepare<Position, Velocity>();
-    auto view = registry.view<Position, Velocity>(persistent_t{});
-
-    printf("creating a ton of entities...\n");
-    auto timePoint = TIME_HERE;
-    for (int i = 0; i < (1<<16); ++i)
-    {
-        auto entity = registry.create();
-        registry.assign<Position>(entity);
-        registry.assign<Velocity>(entity, 0ul, 1ul);
-    }
-    auto elapsed = ELAPSEDuS(timePoint);
-    printf("elapsed: %d\n", elapsed);
-
-    for (int i = 0; i < 10; ++i)
-    {
-        
-    //timePoint = TIME_HERE;
-    //printf("looping a ton of entities, 1 comp...\n");
-    //registry.view<Position>().each([](auto ent, Position& pos)
-    //{
-    //    pos.x = 10;
-    //});
-    //elapsed = ELAPSEDuS(timePoint);
-    //printf("elapsed: %d\n", elapsed);
-
-    printf("looping a ton of entities, 2 comp...\n");
-    timePoint = TIME_HERE;
-
-    view.each([](auto ent, Position& pos, Velocity& vel)
-    {
-        pos.y += vel.y;
-    });
-
-    elapsed = ELAPSEDuS(timePoint);
-    printf("elapsed: %d\n", elapsed);
-
-    }
-
-    printf("finished tests...\n");
-    getchar();
+    Benchmark(100000, false);
+    Benchmark(100000, false);
 }
+
+void Benchmark(int entityCount, bool randomComponents)
+{
+    printf("Benchmarking %d entities, random insertion order: %d", entityCount, randomComponents);
+
+    Registry<int> registry;
+
+    registry.prepare<Int1, Int2, Int3, Int4, Int5, Int6>();
+
+    for (int i = 0; i < entityCount; i++) registry.create();
+
+    vector<int> indices1;
+    vector<int> indices2;
+    vector<int> indices3;
+    vector<int> indices4;
+    vector<int> indices5;
+    vector<int> indices6;
+
+    for (int i = 0; i < entityCount; i += (xorshift(1) + 1)) indices1.push_back(i);
+    for (int i = 0; i < entityCount; i += (xorshift(2) + 1)) indices2.push_back(i);
+    for (int i = 0; i < entityCount; i += (xorshift(3) + 1)) indices3.push_back(i);
+    for (int i = 0; i < entityCount; i += (xorshift(4) + 1)) indices4.push_back(i);
+    for (int i = 0; i < entityCount; i += (xorshift(5) + 1)) indices5.push_back(i);
+    for (int i = 0; i < entityCount; i += (xorshift(6) + 1)) indices6.push_back(i);
+
+    if (randomComponents)
+    {
+        xorshuffle(indices1);
+        xorshuffle(indices2);
+        xorshuffle(indices3);
+        xorshuffle(indices4);
+        xorshuffle(indices5);
+        xorshuffle(indices6);
+    }
+
+    for (int i : indices1)
+    {
+
+    }
+
+    registry.
+
+    for (int i : indices1) registry.assign(registry.EntityUIDFromIdx(i), new Int1(xorshift()));
+    for (int i : indices2) registry.assign(registry.EntityUIDFromIdx(i), new Int2(i, xorshift()));
+    for (int i : indices3) registry.assign(registry.EntityUIDFromIdx(i), new Int3(i, i, xorshift()));
+    for (int i : indices4) registry.assign(registry.EntityUIDFromIdx(i), new Int4(i, i, i, xorshift()));
+    for (int i : indices5) registry.assign(registry.EntityUIDFromIdx(i), new Int5(i, i, i, i, xorshift()));
+    for (int i : indices6) registry.assign(registry.EntityUIDFromIdx(i), new Int6(i, i, i, i, i, xorshift()));
+
+    Measure();
+    registry.Loop((int index, ref Int1 int1, ref Int2 int2) = > { int2.x = int1.x; });
+    Measure("Propagated x to Int2");
+    registry.Loop((int index, ref Int2 int2, ref Int3 int3) = > { int3.x = int2.x; });
+    Measure("Propagated x to Int3");
+    registry.Loop((int index, ref Int3 int3, ref Int4 int4) = > { int4.x = int3.x; });
+    Measure("Propagated x to Int4");
+    registry.Loop((int index, ref Int4 int4, ref Int5 int5) = > { int5.x = int4.x; });
+    Measure("Propagated x to Int5");
+    registry.Loop((int index, ref Int5 int5, ref Int6 int6) = > { int6.x = int5.x; });
+    Measure("Propagated x to Int6");
+
+    registry.Loop((int index, ref Int2 int2, ref Int3 int3, ref Int4 int4) = > { int3.y = int2.y; int4.y = int3.y; });
+    Measure("Propagated y to Int3 and Int4");
+    registry.Loop((int index, ref Int3 int3, ref Int4 int4, ref Int5 int5) = > { int4.y = int3.y; int5.y = int4.y; });
+    Measure("Propagated y to Int4 and Int5");
+    registry.Loop((int index, ref Int4 int4, ref Int5 int5, ref Int6 int6) = > { int5.y = int4.y; int6.y = int5.y; });
+    Measure("Propagated y to Int5 and Int6");
+
+}
+
+//int main()
+//{
+//    printf("starting tests...\n");
+//
+//    Registry<std::uint32_t> registry;
+//    registry.prepare<Position, Velocity>();
+//    auto view = registry.view<Position, Velocity>(persistent_t{});
+//
+//    printf("creating a ton of entities...\n");
+//    auto timePoint = TIME_HERE;
+//    for (int i = 0; i < (1<<16); ++i)
+//    {
+//        auto entity = registry.create();
+//        registry.assign<Position>(entity);
+//        registry.assign<Velocity>(entity, 0ul, 1ul);
+//    }
+//    auto elapsed = ELAPSEDuS(timePoint);
+//    printf("elapsed: %d\n", elapsed);
+//
+//    for (int i = 0; i < 10; ++i)
+//    {
+//        
+//    //timePoint = TIME_HERE;
+//    //printf("looping a ton of entities, 1 comp...\n");
+//    //registry.view<Position>().each([](auto ent, Position& pos)
+//    //{
+//    //    pos.x = 10;
+//    //});
+//    //elapsed = ELAPSEDuS(timePoint);
+//    //printf("elapsed: %d\n", elapsed);
+//
+//    printf("looping a ton of entities, 2 comp...\n");
+//    timePoint = TIME_HERE;
+//
+//    view.each([](auto ent, Position& pos, Velocity& vel)
+//    {
+//        pos.y += vel.y;
+//    });
+//
+//    elapsed = ELAPSEDuS(timePoint);
+//    printf("elapsed: %d\n", elapsed);
+//
+//    }
+//
+//    printf("finished tests...\n");
+//    getchar();
+//}
---
+++ BenchmarkMinECS/Program.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+
+struct Int1
+{
+    public int x;
+    public Int1(int x) { this.x = x; }
+}
+
+struct Int2
+{
+    public int x, y;
+    public Int2(int x, int y) { this.x = x; this.y = y; }
+}
+
+struct Int3
+{
+    public int x, y, z;
+    public Int3(int x, int y, int z) { this.x = x; this.y = y; this.z = z; }
+}
+
+struct Int4
+{
+    public int x, y, z, w;
+    public Int4(int x, int y, int z, int w) { this.x = x; this.y = y; this.z = z; this.w = w; }
+}
+
+struct Int5
+{
+    public int x, y, z, w, v;
+    public Int5(int x, int y, int z, int w, int v) { this.x = x; this.y = y; this.z = z; this.w = w; this.v = v; }
+}
+
+struct Int6
+{
+    public int x, y, z, w, v, u;
+    public Int6(int x, int y, int z, int w, int v, int u) { this.x = x; this.y = y; this.z = z; this.w = w; this.v = v; this.u = u; }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //warmup
+        Benchmark(1000, BufferType.Dense, true);
+        Benchmark(1000, BufferType.Sparse, true);
+        Console.Clear();
+
+        Benchmark(100000, BufferType.Dense, true);
+        Benchmark(100000, BufferType.Sparse, true);
+
+        Benchmark(100000, BufferType.Dense, false);
+        Benchmark(100000, BufferType.Sparse, false);
+        
+        Console.ReadKey();
+
+    }
+
+    private static int state = 42;
+
+    static int xorshift(int maxExclusive = Int32.MaxValue)
+    {
+        int x = state;
+        x ^= x << 13;
+        x ^= x >> 17;
+        x ^= x << 5;
+        state = x;
+        return Math.Abs(x%maxExclusive);
+    }
+
+    static void xorshuffle(List<int> values)
+    {
+        for (int i = 0; i < values.Count; i++)
+        {
+            var swapFor = xorshift(values.Count);
+            var temp = values[i];
+            values[i] = values[swapFor];
+            values[swapFor] = temp;
+        }
+    }
+
+    static Stopwatch sw = new Stopwatch();
+
+    static void Measure(string previousMeasurement = null)
+    {
+        if (previousMeasurement != null) Console.WriteLine($"{sw.Elapsed.TotalMilliseconds:00.00} ms {previousMeasurement}");
+        sw.Restart();
+    }
+    
+    static void Benchmark(int entityCount, BufferType bufferType, bool randomComponents)
+    {
+        Console.WriteLine($"Benchmarking {entityCount} entities, {bufferType.ToString().Split('.').Last()} buffers, random insertion order: {randomComponents}");
+
+        EntityRegistry registry = new EntityRegistry();
+
+        registry.RegisterComponent<Int1>(bufferType);
+        registry.RegisterComponent<Int2>(bufferType);
+        registry.RegisterComponent<Int3>(bufferType);
+        registry.RegisterComponent<Int4>(bufferType);
+        registry.RegisterComponent<Int5>(bufferType);
+        registry.RegisterComponent<Int6>(bufferType);
+
+        for (int i = 0; i < entityCount; i++) registry.CreateEntity();
+
+        List<int> indices1 = new List<int>();
+        List<int> indices2 = new List<int>();
+        List<int> indices3 = new List<int>();
+        List<int> indices4 = new List<int>();
+        List<int> indices5 = new List<int>();
+        List<int> indices6 = new List<int>();
+
+        for (int i = 0; i < entityCount; i+=(xorshift(1) + 1)) indices1.Add(i);
+        for (int i = 0; i < entityCount; i+=(xorshift(2) + 1)) indices2.Add(i);
+        for (int i = 0; i < entityCount; i+=(xorshift(3) + 1)) indices3.Add(i);
+        for (int i = 0; i < entityCount; i+=(xorshift(4) + 1)) indices4.Add(i);
+        for (int i = 0; i < entityCount; i+=(xorshift(5) + 1)) indices5.Add(i);
+        for (int i = 0; i < entityCount; i+=(xorshift(6) + 1)) indices6.Add(i);
+
+        if (randomComponents)
+        {
+            xorshuffle(indices1);
+            xorshuffle(indices2);
+            xorshuffle(indices3);
+            xorshuffle(indices4);
+            xorshuffle(indices5);
+            xorshuffle(indices6);
+        }
+
+        foreach (int i in indices1) registry.AddComponent(registry.EntityUIDFromIdx(i), new Int1(xorshift()));
+        foreach (int i in indices2) registry.AddComponent(registry.EntityUIDFromIdx(i), new Int2(i,xorshift()));
+        foreach (int i in indices3) registry.AddComponent(registry.EntityUIDFromIdx(i), new Int3(i,i,xorshift()));
+        foreach (int i in indices4) registry.AddComponent(registry.EntityUIDFromIdx(i), new Int4(i,i,i,xorshift()));
+        foreach (int i in indices5) registry.AddComponent(registry.EntityUIDFromIdx(i), new Int5(i,i,i,i,xorshift()));
+        foreach (int i in indices6) registry.AddComponent(registry.EntityUIDFromIdx(i), new Int6(i,i,i,i,i,xorshift()));
+
+        Measure();
+        registry.Loop((int index, ref Int1 int1, ref Int2 int2) => { int2.x = int1.x; });
+        Measure("Propagated x to Int2");
+        registry.Loop((int index, ref Int2 int2, ref Int3 int3) => { int3.x = int2.x; });
+        Measure("Propagated x to Int3");
+        registry.Loop((int index, ref Int3 int3, ref Int4 int4) => { int4.x = int3.x; });
+        Measure("Propagated x to Int4");
+        registry.Loop((int index, ref Int4 int4, ref Int5 int5) => { int5.x = int4.x; });
+        Measure("Propagated x to Int5");
+        registry.Loop((int index, ref Int5 int5, ref Int6 int6) => { int6.x = int5.x; });
+        Measure("Propagated x to Int6");
+
+        registry.Loop((int index, ref Int2 int2, ref Int3 int3, ref Int4 int4) => { int3.y = int2.y; int4.y = int3.y; });
+        Measure("Propagated y to Int3 and Int4");
+        registry.Loop((int index, ref Int3 int3, ref Int4 int4, ref Int5 int5) => { int4.y = int3.y; int5.y = int4.y; });
+        Measure("Propagated y to Int4 and Int5");
+        registry.Loop((int index, ref Int4 int4, ref Int5 int5, ref Int6 int6) => { int5.y = int4.y; int6.y = int5.y; });
+        Measure("Propagated y to Int5 and Int6");
+        
+    }
+
+}
+
